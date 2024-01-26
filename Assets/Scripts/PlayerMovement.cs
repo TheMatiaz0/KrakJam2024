c2d58e735cd2081a2e3b5e66d8504656f493f9d7
Szymon Zachara <szymonzachara@gmail.com>
2024-01-27 00:59:41 +0100

    left/right movement 1o1

---
+++ Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,41 @@
+using UnityEngine;
+using UnityEngine.InputSystem;
+namespace KrakJam2024
+{
+    public class PlayerMovement : MonoBehaviour
+    {
+        public const string PLAYER_MOVE = "Player/Walk";
+
+        [SerializeField] private Rigidbody2D _rigidbody;
+        [SerializeField] private PlayerInput _input;
+        [SerializeField] private float _movementMultiply;
+        [SerializeField] private float _movementRead;
+
+        private void OnEnable()
+        {
+            _input.actions[PLAYER_MOVE].Enable();
+        }
+
+        private void OnDisable()
+        {
+            _input.actions[PLAYER_MOVE].Disable();
+        }
+
+        private void Update()
+        {
+            _movementRead = _input.actions[PLAYER_MOVE].ReadValue<float>();
+        }
+
+        private void FixedUpdate()
+        {
+            if (Mathf.Abs(_movementRead) < Mathf.Epsilon)
+            {
+                _rigidbody.AddForce(new Vector2(_rigidbody.velocity.x * (-1f * _movementMultiply * Time.fixedDeltaTime), 0f));
+            }
+            else
+            {
+                _rigidbody.AddForce(new Vector2(_movementRead * _movementMultiply * Time.fixedDeltaTime, 0f));
+            }
+        }
+    }
+}
