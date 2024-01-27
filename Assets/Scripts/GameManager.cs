63bab6b6d691e366ff0543a338fa5f9dc9c4ced4
TheMatiaz0 <TheMatiaz0@protonmail.com>
2024-01-27 20:57:41 +0100

    Add GameManager basic implementation (true statement!)

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace KrakJam2024
+{
+    public class GameManager : MonoBehaviour
+    {
+        public void GameOver()
+        {
+
+        }
+
+        public void Win()
+        {
+
+        }
+    }
+}
