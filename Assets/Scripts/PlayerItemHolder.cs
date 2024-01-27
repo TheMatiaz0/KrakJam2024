596c922a5de4f2b46cfb27a23f8c3cd7154e6957
Szymon Zachara <szymonzachara@gmail.com>
2024-01-27 11:03:28 +0100

    press/release space to throw object

--- Assets/Scripts/PlayerItemHolder.cs
+++ Assets/Scripts/PlayerItemHolder.cs
@@ -4,12 +4,16 @@ namespace KrakJam2024
 {
     public class PlayerItemHolder : MonoBehaviour
     {
+        private const string TAKE_ACTION = "Player/Take";
+
         [SerializeField] private Transform _holdHere;
         [SerializeField] private Item _currentHeldItem;
         [SerializeField] private PlayerInput _input;
         [SerializeField] private float _throwPowerMultiply = 100f;
         [SerializeField] private Animator _animator;
         private PlayerView _playerView;
+        private bool _takenThisFrame;
+        private bool _canThrow;
 
         private Item _lastRegisteredItem;
 
@@ -38,13 +42,14 @@ namespace KrakJam2024
                 _lastRegisteredItem.Take();
                 _currentHeldItem = _lastRegisteredItem;
                 _lastRegisteredItem = null;
+                _takenThisFrame = true;
             }
-            else if (_currentHeldItem != null)
-            {
-                _animator.SetBool("IsHoldingItem", false);
-                _currentHeldItem.Throw(GetThrowVector().normalized * _throwPowerMultiply);
-                _currentHeldItem = null;
-            }
+            // else if (_currentHeldItem != null)
+            // {
+            //     _animator.SetBool("IsHoldingItem", false);
+            //     _currentHeldItem.Throw(GetThrowVector().normalized * _throwPowerMultiply);
+            //     _currentHeldItem = null;
+            // }
         }
 
         private void FixedUpdate()
@@ -52,12 +57,31 @@ namespace KrakJam2024
             if (_currentHeldItem != null)
             {
                 _currentHeldItem.MoveTo(_holdHere);
+                if (_takenThisFrame)
+                {
+                    _takenThisFrame = false;
+                    return;
+                }
+                
+                if (_canThrow && _input.actions[TAKE_ACTION].WasReleasedThisFrame())
+                {
+                    Debug.Log("Released");
+                    _currentHeldItem.Throw(GetThrowVector().normalized * _throwPowerMultiply);
+                    _currentHeldItem = null;
+                    _canThrow = false;
+                }
+                
+                if (_input.actions[TAKE_ACTION].WasPressedThisFrame())
+                {
+                    Debug.Log("Pressed");
+                    _canThrow = true;
+                }
             }
         }
 
         private Vector2 GetThrowVector()
         {
-            var direction = _playerView.LastDirection < 0f? _leftThrow : _rightThrow;
+            var direction = _playerView.LastDirection < 0f ? _leftThrow : _rightThrow;
             return direction.position - _holdHere.position;
         }
     }
